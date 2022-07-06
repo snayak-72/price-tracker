d8eac6031d64bae7f10049fe87214e0f872598cb
Shobhashis Nayak <shobhashis1215@gmail.com>
2022-07-06 21:22:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,69 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import logo from './logo.svg';
 import './App.css';
-import Coin from './Coin';
 
 function App() {
-  const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState('');
-
-  useEffect(() => {
-    axios
-      .get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=1000&page=1&sparkline=false'
-      )
-      .then(res => {
-        setCoins(res.data);
-        console.log(res.data);
-      })
-      .catch(error => console.log(error));
-  }, []);
-
-  const handleChange = e => {
-    setSearch(e.target.value);
-  };
-
-  const filteredCoins = coins.filter(coin =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-  );
-
   return (
-    <div className='coin-app'>
-      <div className='coin-search'>
-        <h1 className='coin-text'>Search a currency</h1>
-        <form>
-          <input
-            className='coin-input'
-            type='text'
-            onChange={handleChange}
-            placeholder='Search'
-          />
-        </form>
-      </div>
-      <div>
-      <ul id="header">
-          <li id="price">price</li>
-          <li id="high">24h high</li>
-          <li id="low">24h low</li>
-          <li id="change">24h</li>
-          <li id="mkt">Mkt Cap</li>
-      </ul> 
-      </div>
-        {filteredCoins.map(coin => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            marketcap={coin.market_cap}
-            high_24h={coin.high_24h}
-            low_24h={coin.low_24h}
-            image={coin.image}
-            priceChange={coin.price_change_percentage_24h}
-          />
-          );
-        })}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
